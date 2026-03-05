2407cc4831b60ee6f2d05641bb6787dbc0b15678
Raphael David <raphaelruizd@gmail.com>
2026-03-05 22:32:12 +1100

    Allow images from external buckets CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,36 +1,37 @@
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => [
-  'strapi::logger',
- {
+  'strapi::errors',
+  {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'connect-src': ["'self'", 'https:'],
           'img-src': [
             "'self'",
             'data:',
             'blob:',
-            'res.cloudinary.com', // cloudinary images
-            'lh3.googleusercontent.com', // google avatars
-            'platform-lookaside.fbsbx.com', // facebook avatars
-            'dl.airtable.com', // strapi marketplace,
-            "market-assets.strapi.io",
-            env('SUPABASE_API_URL'),
+            'https://market-assets.strapi.io',
+            'strapi-ai-staging.s3.us-east-1.amazonaws.com',
+            'strapi-ai-production.s3.us-east-1.amazonaws.com',
+            'https://ltbayhwxivltmrczvadg.supabase.co'
           ],
-          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://ltbayhwxivltmrczvadg.supabase.co'
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/plugins.ts
+++ config/plugins.ts
@@ -3,20 +3,16 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-supabase",
+      provider: 'strapi-provider-upload-supabase-bucket',
       providerOptions: {
-        apiUrl: env("SUPABASE_API_URL"),
-        apiKey: env("SUPABASE_API_KEY"),
-        bucket: env("SUPABASE_BUCKET"),
-        directory: env("SUPABASE_DIRECTORY"),
-        options: {},
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_BUCKET'),
+        directory: env('SUPABASE_DIRECTORY', ''),
+        publicFiles: env.bool('SUPABASE_PUBLIC_FILES', true),
+        signedUrlExpires: env.int('SUPABASE_SIGNED_URL_EXPIRES', 3600),
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-      allowedTypes: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+      sizeLimit: 250 * 1024 * 1024, // 250MB
     },
   },
 });
